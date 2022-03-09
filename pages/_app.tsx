77652ab9d5ba081aadd4d3c3be53b6c4b60f1d19
xc17 <xaliscoral@gmail.com>
2022-03-09 14:50:30 +0700

    Fix: Mantine provider fontfamily

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,11 +10,16 @@ import Router from "next/router";
 import SettingsLayout from "../components/settings/SettingsLayout";
 import { MantineProvider, MantineThemeOverride } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
+import useAppearance from "../hooks/settings/useAppearance";
+import { useEffect } from "react";
 
 function DayDev({ Component, pageProps, ...appProps }: AppProps) {
+    const appearance = useAppearance();
     return (
         <MantineProvider
             theme={{
+                fontFamily: "Poppins",
+                fontFamilyMonospace: "Fira Code",
                 primaryColor: "brand",
                 colors: {
                     brand: [
@@ -26,6 +31,9 @@ function DayDev({ Component, pageProps, ...appProps }: AppProps) {
                         "#16fb71",
                         "#16fb71",
                     ],
+                },
+                other: {
+                    fontFamilySecondary: "Arial",
                 },
             }}
         >
