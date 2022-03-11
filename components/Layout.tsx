93bc1ee20eb964f1aed881ca5f8f586ef5c368f3
qwyzex <qwyzex@gmail.com>
2022-03-11 10:48:06 +0700

    Fix: Layout separate logic

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,57 +1,63 @@
-import { useEffect, useState } from "react";
-import useLocalTheme from "../hooks/settings/useAppearance/useLocalTheme";
 import useUserPresence from "../hooks/useUserPresence";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 import styles from "../styles/Layout.module.sass";
-import useUserDoc from "../hooks/useUserDoc";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../firebase";
-import MobilePlatformWarning from "./MobilePlatformWarning";
-import useLocalSettings from "../hooks/useLocalSettings";
 import useAppearance from "../hooks/settings/useAppearance";
+import { useEffect } from "react";
 
 export interface LayoutInterface {
     children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutInterface) {
-    const userR = useUserPresence();
-    const [user] = useAuthState(auth);
+    const user = useUserPresence();
 
     const appearance = useAppearance();
+
+    const preferences = {
+        theme: user.exists
+            ? appearance.theme === "dark"
+                ? styles.dark
+                : styles.light
+            : "light",
+        fontSize: user.exists
+            ? appearance.fontSize === 100
+                ? styles.font_XL
+                : appearance.fontSize === 75
+                ? styles.font_L
+                : appearance.fontSize === 50
+                ? styles.font_M
+                : appearance.fontSize === 25
+                ? styles.font_S
+                : appearance.fontSize === 0
+                ? styles.font_XS
+                : styles.font_M
+            : styles.font_M,
+        sidebarAlignment: user.exists
+            ? appearance.sidebarAlignment === "right"
+                ? styles.sidebar_right
+                : styles.sidebar_left
+            : "",
+        fontBody: user.exists ? appearance.fontFamily : "Poppins",
+    };
+
+    useEffect(() => {
+        return () => {};
+    }, []);
 
     return (
         <div
             id="__layout"
-            className={`${styles.x} ${
-                appearance.theme === "dark" ? styles.dark : styles.light
-            } ${
-                appearance.fontSize === 100
-                    ? styles.font_XL
-                    : appearance.fontSize === 75
-                    ? styles.font_L
-                    : appearance.fontSize === 50
-                    ? styles.font_M
-                    : appearance.fontSize === 25
-                    ? styles.font_S
-                    : appearance.fontSize === 0
-                    ? styles.font_XS
-                    : styles.font_M
-            } ${
-                appearance.sidebarAlignment === "right"
-                    ? styles.sidebar_right
-                    : styles.sidebar_left
-            }`}
+            className={`${styles.x} ${preferences.fontSize} ${preferences.sidebarAlignment} ${preferences.theme}`}
             style={{
                 // @ts-ignore
-                "--font-body": appearance.fontFamily,
+                "--font-body": preferences.fontBody,
             }}
         >
-            {!userR.loading ? (
+            {!user.loading ? (
                 <>
-                    {userR.userDoc ? <Sidebar /> : <Header />}
+                    {user.exists ? <Sidebar /> : <Header />}
                     <main className={styles.appMainComponent}>{children}</main>
                 </>
             ) : (
