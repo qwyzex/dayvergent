293cfe91f32be1d4084f9eef543dacef5d113012
qwyzex <qwyzex@gmail.com>
2022-03-11 10:53:14 +0700

    Fix: App component cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,11 @@ import "../styles/globals.sass";
 import "../styles/components.sass";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
-import useRedirectUser from "../hooks/useRedirectUser";
-import redirect from "../functions/redirect";
-import Divider from "../components/Divider";
-import Link from "next/link";
-import Router from "next/router";
 import SettingsLayout from "../components/settings/SettingsLayout";
-import { MantineProvider, MantineThemeOverride } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
-import useAppearance from "../hooks/settings/useAppearance";
-import { useEffect } from "react";
 
 function DayDev({ Component, pageProps, ...appProps }: AppProps) {
-    const appearance = useAppearance();
     return (
         <MantineProvider
             theme={{
