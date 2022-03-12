ee82f31d92aad980ffce0316766a6d324242f6de
qwyzex <qwyzex@gmail.com>
2022-03-12 13:46:36 +0700

    Fix: useRedirectUser hooks condition

--- hooks/useRedirectUser.ts
+++ hooks/useRedirectUser.ts
@@ -1,4 +1,3 @@
-import Router from "next/router";
 import useUserPresence from "./useUserPresence";
 import redirect from "../functions/redirect";
 import {
@@ -7,21 +6,25 @@ import {
 } from "../constant/forbiddenPath";
 import { useEffect } from "react";
 import useCurrentRoute from "./useCurrentRoute";
-import redirectIsUser from "../functions/redirectIsUser";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
 
 export default function useRedirectUser() {
-    const USER = useUserPresence();
+    const { userDoc, userData } = useUserPresence();
     const route = useCurrentRoute();
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (!user && route.match(anonymousForbiddenPathRegex)) {
                 return redirect("/login");
-            } else if (user && route.match(userForbiddenPathRegex)) {
+            } else if (
+                user &&
+                userDoc &&
+                userData &&
+                route.match(userForbiddenPathRegex)
+            ) {
                 return redirect("/dashboard");
             }
         });
-    }, [USER.userDoc, route]);
+    }, [userData, userDoc, route]);
 }
