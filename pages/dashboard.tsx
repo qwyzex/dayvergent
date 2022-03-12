71f16bf4f44a52dc28703f296c447e9332215666
qwyzex <qwyzex@gmail.com>
2022-03-12 13:47:04 +0700

    Fix: Clean dashboard imports

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,18 +1,12 @@
-import { collection, doc, getDoc, getDocs, query, updateDoc } from "firebase/firestore";
 import Head from "next/head";
-import Link from "next/link";
-import { useEffect, useState } from "react";
 import TodosLists from "../components/dashboard/TodosLists";
-import Greetings from "../components/Greetings";
 import JournalForm from "../components/journal/Form";
 import TodoForm from "../components/todo/Form";
-import { db } from "../firebase";
-import redirectIsUser from "../functions/redirectIsUser";
 import useJournal from "../hooks/journal/useJournal";
 import useTodos from "../hooks/todo/useTodos";
 import useRedirectUser from "../hooks/useRedirectUser";
 import useUserPresence from "../hooks/useUserPresence";
-import useUserTicket from "../hooks/useUserTicket";
+import styles from "../styles/Dashboard.module.sass";
 
 export default function Dashboard() {
     const user = useUserPresence();
