9daae1579a2501a602c57305aaeedbb1439dd196
qwyzex <qwyzex@gmail.com>
2022-03-12 16:05:55 +0700

    Fix: Mantine colorScheme from useAppearance hooks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,15 @@ import Layout from "../components/Layout";
 import SettingsLayout from "../components/settings/SettingsLayout";
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
+import useAppearance from "../hooks/settings/useAppearance";
 
 function DayDev({ Component, pageProps, ...appProps }: AppProps) {
+    const appearance = useAppearance();
+
     return (
         <MantineProvider
             theme={{
+                colorScheme: appearance.theme === "dark" ? "dark" : "light",
                 fontFamily: "Poppins",
                 fontFamilyMonospace: "Fira Code",
                 primaryColor: "brand",
@@ -22,6 +26,18 @@ function DayDev({ Component, pageProps, ...appProps }: AppProps) {
                         "#16fb71",
                         "#16fb71",
                         "#16fb71",
+                    ],
+                    dark: [
+                        "#cccccc",
+                        "#bbbbbb",
+                        "#aaa9a9",
+                        "#a3a3a3",
+                        "#747474",
+                        "#2f2f2f",
+                        "#2b2b2b",
+                        "#191919",
+                        "#141415",
+                        "#1e1f1e",
                     ],
                 },
                 other: {
